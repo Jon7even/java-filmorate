310189dfab1e9de7488efc589df2ee2078acbfdb
JonSeven <mazahaker5555@yandex.ru>
2023-04-13 21:46:28 +0300

    feat: added a frame of movies ans users. Passed tests 31/67

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,4 +1,40 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
 public class FilmController {
+    private final Map<Integer, Film> films = new HashMap<>();
+    private int idGenerator = 1;
+
+    @GetMapping
+    public List<Film> getAllFilms() {
+        return new ArrayList<>(films.values());
+    }
+
+    @PostMapping
+    public Film addFilm(@RequestBody Film film) {
+        film.setId(idGenerator++);
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilm(@RequestBody Film film) {
+        if (films.containsKey(film.getId())) {
+            films.put(film.getId(), film);
+            return film;
+        } else {
+            System.out.println("Ошибочка вышла");
+            return null;
+        }
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,4 +1,40 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/films")
 public class UserController {
+    private final Map<Integer, User> users = new HashMap<>();
+    private int idGenerator = 1;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return new ArrayList<>(users.values());
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        user.setId(idGenerator++);
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User updateUser(@RequestBody User user) {
+        if (users.containsKey(user.getId())) {
+            users.put(user.getId(), user);
+            return user;
+        } else {
+            System.out.println("Ошибочка вышла");
+            return null;
+        }
+    }
 }
