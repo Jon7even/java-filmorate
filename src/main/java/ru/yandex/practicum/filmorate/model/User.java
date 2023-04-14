bc3bae5771938489d80d6862b07c436bf60d43f6
JonSeven <mazahaker5555@yandex.ru>
2023-04-14 21:06:59 +0300

    feat: revision of validation Film and User. Passed tests 64/3

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,32 +2,25 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 
-import javax.validation.constraints.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
 @Data
 public class User {
     private int id;
 
-    @NotBlank
+    @NotNull
     @Email
     private String email;
 
     @NotBlank
-    @Size(min = 3, max = 20)
     private String login;
 
     private String name;
 
-    @NotNull
     @Past
     private LocalDate birthday;
-
-    public User(int id, String email, String login, String name, LocalDate birthday) {
-        this.id = id;
-        this.email = email;
-        this.login = login;
-        this.name = name;
-        this.birthday = birthday;
-    }
 }
