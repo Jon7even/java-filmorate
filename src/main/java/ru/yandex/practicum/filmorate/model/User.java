48efebde3519ae38105b32d5e94ba948f69f9044
JonSeven <mazahaker5555@yandex.ru>
2023-04-15 20:58:12 +0300

    feat: add Validation of name in UserControl. Passed tests 67/67

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,26 +1,30 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
 @Data
+@RequiredArgsConstructor
 public class User {
     private int id;
 
-    @NotNull
+    @NonNull
     @Email
     private String email;
 
+    @NonNull
     @NotBlank
     private String login;
 
     private String name;
 
+    @NonNull
     @Past
     private LocalDate birthday;
 }
