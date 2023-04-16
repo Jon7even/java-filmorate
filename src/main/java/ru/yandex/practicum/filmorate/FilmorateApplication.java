45f9b67c65ab3739c4caea796fb48b32ab23dbe7
JonSeven <mazahaker5555@yandex.ru>
2023-04-16 16:02:30 +0300

    feat: add Validation for login User

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -3,6 +3,8 @@ package ru.yandex.practicum.filmorate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static ru.yandex.practicum.filmorate.constans.Settings.*;
+
 @SpringBootApplication
 public class FilmorateApplication {
 
