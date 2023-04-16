3a2ae542e1ff4ffadb0aec671c70a70b068aa096
JonSeven <mazahaker5555@yandex.ru>
2023-04-16 16:02:30 +0300

    feat: add Validation for login User

--- src/main/java/ru/yandex/practicum/filmorate/constans/Settings.java
+++ src/main/java/ru/yandex/practicum/filmorate/constans/Settings.java
@@ -10,9 +10,27 @@
 
 package ru.yandex.practicum.filmorate.constans;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.nio.file.Files.lines;
+import static java.nio.file.Paths.get;
 
 public class Settings {
     public static LocalDate SET_MIN_DATE = LocalDate.of(1895, Month.DECEMBER, 28);
+    private static final String HOME = System.getProperty("user.dir") + "/src/main/resources/";
+    public static List<String> BAN_LIST_ADD_LOGIN;
+    public static List<String> BAN_LIST_FIND_LOGIN;
+
+    static {
+        try {
+            BAN_LIST_ADD_LOGIN = lines(get(HOME, "BanListAddLogin.properties")).collect(Collectors.toList());
+            BAN_LIST_FIND_LOGIN = lines(get(HOME, "BanListFindLogin.properties")).collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
